d219ec99160bb4ad783b56f965c9e4a392e70821
CHABANE2000 <121767924+CHABANE2000@users.noreply.github.com>
2024-04-12 18:12:17 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river = document.getElementById('river');
+let boat6 = document.getElementById('boat');
+let nouvil = document.querySelector('.nouvil');
+window.onscroll = function(){
+    let value = scrollY;
+    console.log(value);
+    stars.style.left = value + 'px';
+    moon.style.top = value * 3 + 'px';
+    mountains3.style.top = value * 2 + 'px';
+    mountains4.style.top = value  + 'px';
+    river.style.top = value  + 'px';
+    boat6.style.top = value  + 'px';
+    boat6.style.left = value * 3  + 'px';
+    nouvil.style.fontSize = value * 4 + 'px';
+}
